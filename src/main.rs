2d33ce7bc2615c8f924124a448b756c143d0bcef
rhusiev <63148080+rhusiev@users.noreply.github.com>
2025-01-03 22:14:03 +0200

    add basic rss handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use feed_rs::parser;
+
+#[tokio::main]
+async fn main() {
+    // let example_rss = reqwest::get("https://www.liga.net/newsua/all/rss.xml")
+    //     .await
+    //     .unwrap()
+    //     .text()
+    //     .await
+    //     .unwrap();
+    // fs::write("example.rss", example_rss.clone()).unwrap();
+
+    let example_rss = fs::read_to_string("example/example.rss").unwrap();
+
+    let parser = parser::Builder::new().sanitize_content(true).build();
+    let feed = match parser.parse(example_rss.as_bytes()) {
+        Ok(feed) => feed,
+        Err(err) => panic!("{}", err),
+    };
+
+    println!("{:#?}", feed);
 }
